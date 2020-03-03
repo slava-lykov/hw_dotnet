d50998ee3be643c58035149dda3b0a1783576256
Viachaslau Lykau <v.lykau@corp.sdventures.com>
2020-03-03 13:22:39 +0300

    2nd hw

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StreamReader textReader = new StreamReader(@"C:\image.txt", true);
+
+            string textReaderResult = textReader.ReadToEnd();
+
+            textReader.Dispose();
+                        
+            string[] arrayOfTextResult = textReaderResult.Split(' ');
+
+            for (int i = 0; i < arrayOfTextResult.Length - 1; i++)
+
+                {
+
+                byte binary = Convert.ToByte(arrayOfTextResult[i], 2);
+
+                byte[] imageBytes = new byte[arrayOfTextResult.Length - 1];
+                
+                imageBytes[i] = binary;
+
+                File.WriteAllBytes(@"\\SDV\DFS\MINBO\USERS\v.lykau\Desktop\autotests\image.png", imageBytes);
+
+                };
+                                 
+                       
+                              
+        }
+    }
+}
